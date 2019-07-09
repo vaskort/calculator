3b84aa5dda7080b287539c3fcadb8e7fd98a302e
Vasilis Kortsimelidis <vaskort@gmail.com>
2019-07-10 00:58:41 +0100

    Styling updates

--- README.md
+++ README.md
@@ -29,3 +29,7 @@ npm start
 ```
 
 The app should be loaded on http://localhost:3000/
+
+```
+Version: 627d7be039e0085025a51d47e42bdd970409ec1c
+```
--- src/components/Buttons/Buttons.jsx
+++ src/components/Buttons/Buttons.jsx
@@ -5,13 +5,10 @@ import styles from './Buttons.module.css';
 const Buttons = ({ onPressNumber, onClear, onEquals, onPressCalculation }) => {
   const numbers = [1,2,3,4,5,6,7,8,9]
   return (
-    <div>
-      <div className={styles.calcContainer}>
+    <div className={styles.buttonsContainer}>
         <button className={styles.button} onClick={onPressCalculation} value={"+"}>+</button>
         <button className={styles.button} onClick={onPressCalculation} value={"-"}>-</button>
         <button className={styles.button} onClick={onPressCalculation} value={"/"}>/</button>
-        <button className={styles.button} onClick={onEquals}>=</button>
-      </div>
       {numbers.map(number => (
         <button 
           key={number} 
@@ -25,6 +22,7 @@ const Buttons = ({ onPressNumber, onClear, onEquals, onPressCalculation }) => {
       <button className={styles.button} onClick={onClear}>
         Clear
       </button>
+      <button className={styles.button} onClick={onEquals}>=</button>
     </div>
   );
 };
